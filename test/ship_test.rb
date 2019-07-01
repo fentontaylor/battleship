d9cf760a6f04c310cb31010b65dd5ee782e4cb58
Daniel <danmoran.pro@gmail.com>
2019-07-01 17:20:39 -0600

    Add ship class and test for attributes and ship objects.
    Co-authored-by: Fenton Taylor <18686466+fentontaylor@users.noreply.github.com>

---
+++ test/ship_test.rb
@@ -0,0 +1,35 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/ship"
+
+class ShipTest < Minitest::Test
+
+  def setup
+    @cruiser = Ship.new("Cruiser", 3)
+
+  end
+
+
+  def test_ship_exist
+    assert_instance_of Ship, @cruiser
+  end
+
+  def test_attributes
+    assert_equal "Cruiser", @cruiser.name
+
+    assert_equal 3, @cruiser.length
+
+    assert_equal 3, @cruiser.health
+
+    refute @cruiser.sunk?
+
+end
+  end
+
+
+
+
+
+
+
+end

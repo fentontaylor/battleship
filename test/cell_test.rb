bbce422e63c0e6e31a3632c99df864f17c0624c5
Fenton Taylor <fentontaylor@gmail.com>
2019-07-02 15:29:31 -0600

    Tests pass for .coordinate, .ship, .fired_upon, .empty?, .fired_upon?, .place_ship
    Co-authored-by: Daniel Moran <9169813+danmoran-pro@users.noreply.github.com>

---
+++ test/cell_test.rb
@@ -0,0 +1,36 @@
+require './lib/cell'
+require './lib/ship'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class CellTest < Minitest::Test
+
+  def setup
+    @cell = Cell.new("B4")
+    @cruiser = Ship.new("Cruiser", 3)
+
+  end
+
+  def test_cell_exists
+    assert_instance_of Cell, @cell
+  end
+
+  def test_attributes
+    assert_equal "B4", @cell.coordinate
+    assert_nil @cell.ship
+  end
+
+  def test_empty?
+    assert @cell.empty?
+  end
+
+  def test_place_ship
+    @cell.place_ship(@cruiser)
+    assert_instance_of Ship, @cell.ship
+    refute @cell.empty?
+  end
+
+  def test_fired_upon?
+    refute @cell.fired_upon?
+  end
+end

1ad156e9bacf6654a784a2bd1d53846b8e20dcc1
Fenton Taylor <fentontaylor@gmail.com>
2019-07-02 15:43:23 -0600

    Pass test for fire_upon
    Co-authored-by: Daniel Moran <9169813+danmoran-pro@users.noreply.github.com>

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -33,4 +33,11 @@ class CellTest < Minitest::Test
   def test_fired_upon?
     refute @cell.fired_upon?
   end
+
+  def test_fire_upon
+    @cell.place_ship(@cruiser)
+    @cell.fire_upon
+    assert_equal 2, @cell.ship.health
+    assert @cell.fired_upon?  
+  end
 end

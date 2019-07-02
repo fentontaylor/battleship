29037c4c6e267cd710a616a20e0b6479117e0d1d
Fenton Taylor <fentontaylor@gmail.com>
2019-07-02 15:27:22 -0600

    Build Cell class and methods: .coordinate, .ship, .fired_upon?, .empty?, .place_ship
    Co-authored-by: Daniel Moran <9169813+danmoran-pro@users.noreply.github.com>

---
+++ lib/cell.rb
@@ -0,0 +1,22 @@
+class Cell
+
+  attr_reader :coordinate, :ship
+
+  def initialize(coordinate)
+    @coordinate = coordinate
+    @ship = nil
+    @fired_upon = false
+  end
+
+  def empty?
+    @ship.nil?
+  end
+
+  def fired_upon?
+    @fired_upon
+  end
+
+  def place_ship(ship)
+    @ship = ship
+  end
+end

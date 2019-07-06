ea32afe0db3f9f7a6bc061fa6156932a82f7d499
Daniel <danmoran.pro@gmail.com>
2019-07-06 11:07:08 -0600

    Add game test and game file.
    Co-authored-by: Fenton Taylor <18686466+fentontaylor@users.noreply.github.com>

---
+++ test/game_test.rb
@@ -0,0 +1,18 @@
+require './lib/cell'
+require './lib/ship'
+require './lib/board'
+require './lib/game'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class GameTest < Minitest::Test
+
+  def setup
+    @game = Game.new
+  end
+
+  def test_it_exists
+    assert_instance_of Game, @game
+  end
+
+end

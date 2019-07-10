50d22cf5bf93099c1acfd26acf67bb61fa9eeb0e
Daniel <danmoran.pro@gmail.com>
2019-07-10 15:06:25 -0600

    Add test all_ship_sunk?. cut and pasted play method to runner file.
    Co-authored-by: Fenton Taylor <18686466+fentontaylor@users.noreply.github.com>

--- test/game_test.rb
+++ test/game_test.rb
@@ -2,6 +2,8 @@ require './lib/cell'
 require './lib/ship'
 require './lib/board'
 require './lib/game'
+require './lib/human_player'
+require './lib/computer_player'
 require 'minitest/autorun'
 require 'minitest/pride'
 require 'pry'
@@ -15,5 +17,10 @@ class GameTest < Minitest::Test
   def test_it_exists
     assert_instance_of Game, @game
   end
+
+  def test_all_ships_sunk?
+    refute @game.all_ships_sunk?(@game.player.ships)
+  end
+
 
 end
